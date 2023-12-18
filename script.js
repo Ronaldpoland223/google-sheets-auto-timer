7654f4bfb11df36982bbc70d3d35eedf2e4c7599
Ronald Poland <154252839+Ronaldpoland223@users.noreply.github.com>
2023-12-18 17:56:02 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+   "dynamicLink": {
+      "baseURL": "https://pylons.page.link",
+      "apn": "tech.pylons.wallet",
+      "ibi": "xyz.pylons.wallet",
+      "isi": "1598732789",
+      "oflPlay": "https://play.google.com/store/apps/details?id=tech.pylons.wallet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1",
+      "oflIOS": "https://apps.apple.com/us/app/pylons/id1598732789"
+    },
+    "coins": [
+      {
+        "denom": "uatom",
+        "displayName": "ATOM",
+        "fraction": 1000000
+      },
+      {
+        "denom": "ubedrock",
+        "displayName": "UBEDROCK",
+        "fraction": 1000000
+      },
+      {
+
+      "@organisms": ["@next/components/organisms"],
+      "@templates": ["./@next/components/templates"],
+      "@pages": ["./@next/pages"],
+      "@layouts": ["./@next/layouts"]
+    }
+  },
+  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx", "types.d.ts"],
+  "exclude": ["node_modules"]
